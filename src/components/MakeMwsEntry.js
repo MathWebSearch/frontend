7086fd93d07dc1557f9014aa174a7ac016f91192
Christoph Alt <christoph.alt@fau.de>
2019-06-10 00:15:42 +0200

    some first tries with the xpath stuff

--- src/components/MakeMwsEntry.js
+++ src/components/MakeMwsEntry.js
@@ -1,58 +1,75 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
+// import {find} from 'xpath-react';
+// import {evaluate, XPathResult} from 'xpath-react';
 
 function extractFormula(hit) {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(hit.xhtml, "text/html");
-    const math_tags = htmlDoc.getElementsByTagName('m:math');
-    const local_id = hit.math_ids[0].url;
-    let i = 0;
-    while(i < math_tags.length){
-        if(math_tags[i].getAttribute('local_id') === local_id){
-            break;
-        }
-        i++;
-    }
+  // console.log(hit);
+  const local_id = hit.math_ids[0].url;
+  // not sure if thats the most clever way, maybe there are some side effects
+  const math_tags = ReactHtmlParser(hit.xhtml.replace(/m:/g, ''));
+  let math = math_tags.find(
+    e => e.type === 'math' && e.props.local_id === local_id,
+  );
+  if (!math) {
+    return;
+  }
+  // TODO find a way to highlight the xpath
+  const xpath = hit.math_ids[0].xpath;
+  // let expr = find(math, `./${xpath}`);
+  // expr = React.cloneElement(expr, {background: 'yellow'});
+  // const xmlid = expr.props['xml:id'];
+  // console.log(expr);
+  // const qvar = math.props.children.find(e => e.props["xml:id"] === xmlid);
+  // console.log(qvar);
+  // const result = evaluate(`./${xpath}`, math, null, XPathResult.ANY_TYPE);
+  // console.log(result);
 
-    const url = math_tags[i].getAttribute('url');
-    const formula = math_tags[i].outerHTML.replace(/m:/g,'');
-    return ( <span key={local_id}>
-        <div> {ReactHtmlParser(formula)} </div>
-                <a href={url} target="_blank" rel="noopener noreferrer"
-                    onClick={(ev) => {ev.stopPropagation();
-                    /*keeps it active even if clicked the link*/}}>
-                    Go to nLab
-                </a>
-            </span>
-    );
+  const url = math.props.url;
+  const xmlns = {xmlns: 'http://www.w3.org/1998/Math/MathML'};
+  return (
+    <div className="Content" key={local_id.toString() + xpath} {...xmlns}>
+      <div>{math}</div>
+      {/* <div>{expr} </div> */}
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={ev => {
+          ev.stopPropagation();
+          /*keeps it active even if clicked the link*/
+        }}>
+        Go to nLab
+      </a>
+    </div>
+  );
 }
-export function MakeEntries(hits, allEntries){
-
-    for(let i = 0; i < hits.length; i++ ){
-        const parser = new DOMParser();
-        const htmlDoc = parser.parseFromString(hits[i].xhtml, "text/html");
-        const id_tags = htmlDoc.getElementsByTagName('id');
-        if(!id_tags || !id_tags.length){
-            // ignore all hits without an id tag
-            console.log('hit without a id');
-            continue;
-        }
-        const key = id_tags[0].innerHTML;
-        if(!allEntries[key]){
-            const metadata = htmlDoc.getElementsByTagName('metadata')[0];
-            const title = metadata.getElementsByTagName('title')[0].innerHTML;
-            allEntries[key] = {
-                key: key,
-                title: title,
-                active: false,
-                formulas: []
-            };
-        }
-        // this prevents that the same formula appears two times in the list
-        // at this point we don not use the xpath thing so there is no point in
-        // showing the same formula twice
-        const newMath = extractFormula(hits[i]);
-        allEntries[key]['formulas'].every((e) => e.key !== newMath.key)
-            && allEntries[key]['formulas'].push(newMath);
+export function MakeEntries(hits, allEntries) {
+  for (let i = 0; i < hits.length; i++) {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(hits[i].xhtml, 'text/html');
+    const id_tags = htmlDoc.getElementsByTagName('id');
+    if (!id_tags || !id_tags.length) {
+      // ignore all hits without an id tag
+      console.log('hit without a id');
+      continue;
     }
+    const key = id_tags[0].innerHTML;
+    if (!allEntries[key]) {
+      const metadata = htmlDoc.getElementsByTagName('metadata')[0];
+      const title = metadata.getElementsByTagName('title')[0].innerHTML;
+      allEntries[key] = {
+        key: key,
+        title: title,
+        active: false,
+        formulas: [],
+      };
+    }
+    // this prevents that the same formula appears two times in the list
+    // at this point we don not use the xpath thing so there is no point in
+    // showing the same formula twice
+    const newMath = extractFormula(hits[i]);
+    allEntries[key]['formulas'].every(e => e.key !== newMath.key) &&
+      allEntries[key]['formulas'].push(newMath);
+  }
 }
