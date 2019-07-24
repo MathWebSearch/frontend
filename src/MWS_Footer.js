af93d1a7b859730890988b697080c6481ba8ae11
Christoph Alt <christoph.alt@fau.de>
2019-07-24 22:32:32 +0200

    header and footer

--- src/MWS_Footer.js
+++ src/MWS_Footer.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import './css/MWS_Footer.css'
+import './css/MWS_Footer.css';
+import kwarc from './img/kwarc_logo.png'
+import fau from './img/fau_logo.png'
 
-export function MwsFooter(){
-    return <div className="Footer"><br /> really nice footer </div>;
+export function MwsFooter() {
+  return (
+    <div className="Footer">
+      <div >
+        Powered by <a href="http://search.mathweb.org/">MathWebSearch</a>
+      </div>
+      <div className="footer">
+        <a href="http://kwarc.info/" className="footlink">
+            <img src={kwarc} alt="KWARC research group" />
+        </a>
+        <a href="https://fau.de" className="footlink">
+            <img src={fau}alt="FAU Erlangen-Nürnberg" />
+        </a>
+      </div>
+    </div>
+  );
 }
