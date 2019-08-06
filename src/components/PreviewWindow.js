c5c7abe5ed212b4ee3ba4ff22ccea81ba9ea66fd
Christoph Alt <christoph.alt@fau.de>
2019-08-06 08:55:19 +0200

    qvars in preview are now also colored

--- src/components/PreviewWindow.js
+++ src/components/PreviewWindow.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../css/PreviewWindow.css';
 import {MathML} from './MathML';
+import {colors} from '../util/Colors';
 
 export function PreviewWindow(props) {
-  const {mathstring} = props;
+  let {mathstring} = props;
   if ('' === mathstring) {
     return (
       <div className="PreviewWindow">
@@ -12,6 +13,13 @@ export function PreviewWindow(props) {
       </div>
     );
   }
+  // this colors the qvar in the right color
+  // it depends that hopfully nothing else has the attribute mathcolor = red
+  let i = 0;
+  mathstring = mathstring.replace(
+    /mathcolor="red"/g,
+    () => `mathcolor="${colors[i++ % colors.length]}"`,
+  );
   return (
     <div className="PreviewWindow">
       <MathML mathstring={mathstring} />
