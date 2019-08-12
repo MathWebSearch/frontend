a282127f22bd54f7a4121669d690d9ce911f65ec
Christoph Alt <christoph.alt@fau.de>
2019-08-12 11:56:07 +0200

    dissabled mathjax, it stopped working for some unknow reason

--- src/components/MathML.js
+++ src/components/MathML.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import MathJax from 'react-mathjax-preview';
+// import MathJax from 'react-mathjax-preview';
 import '../css/MathML.css';
 
 export class MathML extends React.Component {
   render() {
-    const agent = navigator.userAgent;
-    const useMathJax = !(
-      (agent.indexOf('Gecko') > -1 && agent.indexOf('KHTML') === -1) ||
-      agent.match(/MathPlayer/)
-    );
+    // const agent = navigator.userAgent;
+    // const useMathJax = !(
+    //   (agent.indexOf('Gecko') > -1 && agent.indexOf('KHTML') === -1) ||
+    //   agent.match(/MathPlayer/)
+    // );
     let pmml = this.props.mathstring.replace(/m:/g, '');
     if (!pmml) {
       return;
     }
-    if (useMathJax) {
-      //cut out alle the annotations that mathjax works
-      pmml = pmml.replace(/<annotation[\s\S]*>[\s\S]*<\/annotation[\S]*>/g, '');
-      return <MathJax className="Math" math={pmml} />;
-    }
+    //if (useMathJax) {
+    //  //cut out alle the annotations that mathjax works
+    //  pmml = pmml.replace(/<annotation[\s\S]*>[\s\S]*<\/annotation[\S]*>/g, '');
+    //  return <MathJax className="Math" math={pmml} />;
+    //}
 
     return <div className="Math" dangerouslySetInnerHTML={{__html: pmml}} />;
   }
