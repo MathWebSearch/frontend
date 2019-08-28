e9b11e93965f6bde60caf4ccf2ed5bbf6590be35
Christoph Alt <christoph.alt@fau.de>
2019-08-28 10:06:35 +0200

    updated text in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,15 +51,23 @@ export class Header extends React.Component {
         <p style={{marginTop: '20px'}}>
           Enter a comma-separated list of key phrases into the top search bar
           and a set of formulae schemata (written in LaTeX with ?a, ?b, ... for
-          query variables; they are marked in red in the formula preview). To
-          add a numeric range add \range{'{lower}'}
-          {'{high}'} or \relrange{'{base}'}
-          {'{deviation}'}
+          query variables; they are marked in different colors in the formula
+          preview). To add a numeric range insert{' '}
+          <b>
+            \range{'{lower}'}
+            {'{high}'}
+          </b>{' '}
+          or
+          <b>
+            {' '}
+            \relrange{'{base}'}
+            {'{deviation}'}
+          </b>{' '}
           (this adds a interval from [base - base*deviation%, base +
-          base*deviation%]). A formula schema in a query matches any formula in
-          the MWS index that has an instance schema as a subformula. Query
-          variables with the same name must be instantiated with the same
-          formula, see the examples for inspiration.{' '}
+          base*deviation%]) in your search query. A formula schema in a query
+          matches any formula in the MWS index that has an instance schema as a
+          subformula. Query variables with the same name must be instantiated
+          with the same formula, see the examples for inspiration.{' '}
           <a href={'https://github.com/KWARC/mws/wiki'}>... more</a>
         </p>
       </div>
