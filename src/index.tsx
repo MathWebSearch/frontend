ca6aeb4e4b2c72ee4dd8c7b235ae398d6a602860
Christoph Alt <christoph.alt@fau.de>
2019-10-22 16:15:27 +0200

    color theme is now done with css variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './Themes/theme1.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
